ca56281ded6753cb98b015b9ff314feabd8b61ee
Bamzy123 <stephenomotos@gmail.com>
2025-07-10 20:40:28 +0100

    update on Options.js, Option.js, Question.js and Game.js model.

---
+++ models/User.js
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: [true, 'Username is required'],
+        trim: true,
+        unique: true,
+        minlength: [3, 'Username must be at least 3 characters long'],
+        maxlength: [20, 'Username cannot exceed 20 characters']
+    },
+    
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        trim: true,
+        minlength: [8, 'Password must be at least 8 characters long'],
+        select: false
+    }
+});
+
+userSchema.index({ userName: 1 });
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    try {
+        return await bcrypt.compare(candidatePassword, this.password);
+    } catch (error) {
+        throw error;
+    }
+};
+
+userSchema.methods.toSafeObject = function() {
+    const { password, ...safeUser } = this.toObject();
+    return safeUser;
+};
+
+const User = mongoose.model('User', userSchema);
+export default User;
