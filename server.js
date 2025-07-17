6580bb7002c723ff271a302cef376126e0d66dd8
christianahOlude <oludechristianah16@gmail.com>
2025-07-17 19:15:44 +0100

    update

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ const app = express();
 connectDB();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use('/api/auth', router);
 app.use('/api/user', userRouter);
